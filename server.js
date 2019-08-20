08549dd53d267bbc98db3819d8ff30210ade42dc
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-08-21 11:52:32 +1200

    endpoints

--- server.js
+++ server.js
@@ -13,7 +13,22 @@ app.get('/', function(req, res){
     res.send('Welcome to our Products API. Use endpoints to filter out the data');
 });
 
+app.get('/allProducts', function(req, res) {
+  res.send(allProducts);
+});
 
+app.get('/product/:id', function(req, res) {
+  let product;
+  const productIdParam = req.params.id;
+  res.send(productIdParam);
+  for (var i = 0; i < allProducts.length; i++) {
+    if (allProducts[i].id == productIdParam) {
+      product = allProducts[i];
+      res.send(product); // [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client error message
+    }
+  }
+
+});
 
 app.listen(port, () => {
     console.clear();
