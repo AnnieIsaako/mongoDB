de2994466c7fcc336da735c61dbb5ea9d48679bd
AnnieIsaako <annie@gmail.com>
2019-08-21 22:28:24 +1200

    This is the server that responds with the correct id

--- server.js
+++ server.js
@@ -20,17 +20,44 @@ app.get('/allProducts', function(req, res) {
 app.get('/product/:id', function(req, res) {
   let product;
   const productIdParam = req.params.id;
-  res.send(productIdParam);
+  // res.send(productIdParam);
   for (var i = 0; i < allProducts.length; i++) {
     if (allProducts[i].id == productIdParam) {
       product = allProducts[i];
-      res.send(product); // [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client error message
+      break;
     }
   }
-
+  res.send(product);
 });
 
+app.get('/product/edit/:id', function(req, res) {
+  let product;
+  const productEditId = req.params.id;
+  // res.send(productIdParam);
+  for (var i = 0; i < allProducts.length; i++) {
+    if (allProducts[i].id == productEditId) {
+      product = allProducts[i];
+      break;
+    }
+  }
+  res.send(product);
+});
+
+app.get('/product/delete/:id', function(req, res) {
+  let product;
+  const productDeleteId = req.params.id;
+  // res.send(productIdParam);
+  for (var i = 0; i < allProducts.length; i++) {
+    if (allProducts[i].id == productDeleteId) {
+      product = allProducts[i];
+      break;
+    }
+  }
+  res.send(product);
+});
+
+
 app.listen(port, () => {
-    console.clear();
+    // console.clear();
     console.log(`application is running on port ${port}`)
 });
